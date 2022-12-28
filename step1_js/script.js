5769070b17e6e8dda780e6344d3a89f753953453
tim6400 <ch.ginz@gmail.com>
2022-12-28 20:00:02 +0200

    28-12-22

--- step1_js/script.js
+++ step1_js/script.js
@@ -1,56 +1,57 @@
-// console.log("ss")
-
-$("#myDiv").css("border", "3px solid red");
-
 
 var TasksArray = new Array();
 var val = document.getElementById('Task');
-var active = document.getElementById('active');
 
-
-
-// var checkboxes = document.querySelectorAll("input[type=checkbox][name=settings]");
-
-// // Select all checkboxes with the name 'settings' using querySelectorAll.
-// let enabledSettings = []
-
-// checkboxes.forEach(function(checkbox) {
-//   checkbox.addEventListener('change', function() {
-//     enabledSettings = 
-//       Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
-//       .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
-//       .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
-
-//     console.log(enabledSettings)
-//   })
-// });
-
-
-const wrapper = document.getElementById('list');
-wrapper.addEventListener('click', (event) => {
+document.getElementById('list').addEventListener('click', (event) => {
     var checked = event.srcElement.checked;
+    let index=TasksArray.findIndex(x=>x.id==event.srcElement.id[0]);
+    TasksArray[index].active=checked==true?"checked='true'":"";;
     if (checked == true) {
-        $('#' + event.srcElement.id[0] + 'inp').addClass("line-through");
+        $('#' + event.srcElement.id[0] ).addClass("line-through");
     } else {
-        $('#' + event.srcElement.id[0] + 'inp').removeClass("line-through");
-    }
-    // const isButton = event.target.nodeName === 'BUTTON';
-    // if (!isButton) {
-    //   return;
-    // }
+        $('#' + event.srcElement.id[0]).removeClass("line-through");
+    } 
 
-    // console.dir(event.target.id);
 })
+function add()
+{
+    TasksArray.push({ val: val.value, active:"", id: TasksArray.length + 1 });
+    UpdatedList(TasksArray);
+}
+
+function filter(index)
+{
+    switch(index) {
+        case 1:
+            UpdatedList(TasksArray);
+          break;
+        case 2:
+            UpdatedList(TasksArray.filter(x=>x.active==false));
+          break;
+          case 3:
+            UpdatedList(TasksArray.filter(x=>x.active=="checked='true'"));
+          break;
+        default:
+      }   
+}
+
+
 
 document.querySelector('#Task').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        TasksArray.push({ val: val.value, active: active.checked ? "checked='true'" : "", id: TasksArray.length + 1 });
-        console.log(TasksArray);
-        document.getElementById('Task').value = "";
-        document.getElementById('active').checked = false;
-        $("#list").empty();
-        for (var i = 0; i < TasksArray.length; i++) {
-            $("#list").append($("<tr><td>  <input " + TasksArray[i].active + " name='settings' type=checkbox id=" + TasksArray[i].id + "check" + " >").append("</td><span id=" + TasksArray[i].id + "inp" + "> &nbsp;" + TasksArray[i].val + "</span></tr>"))
-        }
+        TasksArray.push({ val: val.value, active:"", id: TasksArray.length + 1 });
+        UpdatedList(TasksArray);
     }
 });
+
+function UpdatedList(list){
+    document.getElementById('Task').value = "";
+    $("#list").empty();
+    for (var i = 0; i < list.length; i++) {
+        $("#list").append($("<tr><td>  <input " + list[i].active + " name='settings' type=checkbox id=" + list[i].id + "check" + " >").append("</td><span id=" + list[i].id  + "> &nbsp;" + list[i].val + "</span></tr>"))
+    if(list[i].active!=""){
+        $('#'+list[i].id ).addClass("line-through");
+    }
+
+    } 
+}
