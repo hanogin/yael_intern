e97a897be2199018568e67d92750f0ec12c596c6
tim6400 <ch.ginz@gmail.com>
2022-12-28 12:54:52 +0200

    28.12.22

--- step1_js/script.js
+++ step1_js/script.js
@@ -1,3 +1,29 @@
 console.log("ss")
 
 $( "#myDiv" ).css( "border", "3px solid red" );
+
+
+var TasksArray = new Array();
+var val = document.getElementById('Task');
+var active = document.getElementById('active');
+
+
+document.querySelector('#Task').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        TasksArray.push({val:val.value,active:active.checked, id:TasksArray.length+1});
+        console.log(TasksArray);
+        document.getElementById('Task').value="";
+        document.getElementById('active').checked=false;
+
+       $("#list").empty();
+
+for(var i = 0; i <TasksArray.length; i++) {
+ $("#list").append($("<td>  <input type=checkbox id=" + TasksArray[i].id + ">").append("</td><td><input type='text' id=" + TasksArray[i].id + ">"+"</td>"))
+
+}
+
+    }
+});
+
+
+  
